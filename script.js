63b59198a370cf8f596005f5475acf9bd147a3dc
Aniket Kule <38484786+Aniket230297@users.noreply.github.com>
2023-04-23 13:49:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function check(string) {
+	for (var i = 0; i < string.length; i++) {
+    var c = string.charAt(i);
+    if (string.indexOf(c) == i && string.indexOf(c, i + 1) == -1) {
+      return c;
+    }
+  }
+  return null;
+}
+const string = prompt("Enter string.");
+alert(c);
